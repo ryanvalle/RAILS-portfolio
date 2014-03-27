588227e1bbe39e01a3ed2080bba89807f8e3e358
vallr034 <ryan.valle@disney.com>
2014-03-26 20:25:38 -0700

    added video section

--- app/helpers/portfolios_helper.rb
+++ app/helpers/portfolios_helper.rb
@@ -1,5 +1,18 @@
 module PortfoliosHelper
 
+	def put_content(source)
+		case source.mediatype.downcase
+		when "photography"
+			image_tag(return_content(source), class:"img-responsive")
+		when "video"
+			p "<div class='container'><iframe id='ytplayer' type='text/html' width='100%' height='390'
+  src='http://www.youtube.com/embed/#{source.location}?autoplay=1'
+  frameborder='0'></iframe></div>".html_safe
+		when "web"
+			# render web
+		end
+	end
+
 	def return_content(source)
 		p source.location
 	end
